8dd56da3466e38f129c923736bf6eb779ff8f985
pran0112 <55594298+pran0112@users.noreply.github.com>
2019-12-03 23:03:01 +0530

    Add files via upload

---
+++ note.py
@@ -0,0 +1,32 @@
+import msvcrt
+import mysql.connector
+#connecting to my database
+try:
+    print("CONNECTING TO DATABSE...")
+    user=input("User: ")
+    passwd=input("Password: ")
+    mydb=mysql.connector.connect(host="localhost",user=user,passwd=passwd,database="notes")
+    print("""Welcome To my note taking program.
+    Instructions:
+    You can keep writing your notes even if you press ENTER for a new line.
+    When you want the program to stop taking note, press "ctrl+S" in a new line.
+    When asked, enter the name you want to save the note as.""")
+    print("\nWrite your note:")
+    notedb= ""
+    while True:
+        notedb+=input()
+        c=msvcrt.getch() #getting the key pressed without pressing enter. It returns the byte.
+        if ord(c)==19:   #ascii value of ctrl+s
+            print("###SAVED###")
+            break
+        elif (97<=ord(c) and ord(c)<=122) or (65<=ord(c) and ord(c)<=90):  #if its either in (a to z) or(A to Z)
+            notedb+= "\n" + chr(ord(c))
+            print(chr(ord(c)),end="")
+        else:
+            notedb+= "\n"
+    namedb=input("Enter name of note: ")
+    cursor=mydb.cursor() #adding an sql cursor to execute sql queries
+    cursor.execute("INSERT INTO mynotes(name,note) VALUES(%s,%s)",(namedb,notedb)) #executing any sql querry
+    mydb.commit() #always needed to save the last executed query
+except :
+    print("##USER OR PASSWORD IS INCORRECT.##")
